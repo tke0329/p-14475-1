69b14d54333179e0d48113bf4a3b746f40828926
ysman <tke0329@gmail.com>
2026-04-20 23:47:13 +0900

    0001/git_오류로_인한_일괄커밋

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,13 +1,63 @@
 package com.ll;
 
+import java.util.*;
+
 public class Calc {
-    public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+    public static int run(String value) {
 
-        int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
+        List<String> valueList = new ArrayList<>();
 
-        return num1 + num2;
+        value = calcBracket(value);
+        inputList(valueList, value);
+
+
+        for (int i = 0; i < valueList.size(); i++) {
+
+            if (valueList.get(i).equals("*")) {
+                int left = Integer.parseInt(valueList.get(i - 1));
+                int right = Integer.parseInt(valueList.get(i + 1));
+
+                valueList.remove(i - 1);
+                valueList.remove(i - 1);
+                valueList.remove(i - 1);
+                valueList.add(i - 1, String.valueOf(left * right));
+                i--;
+            }
+        }
+
+        int result = Integer.parseInt(valueList.getFirst());
+        for (int j = 1; j < valueList.size(); j += 2) {
+            String op = valueList.get(j);
+            int next = Integer.parseInt(valueList.get(j + 1));
+            if (op.equals("+")) result += next;
+            else if (op.equals("-")) result -= next;
+        }
+
+        return result;
+
+
     }
+
+    public static String calcBracket(String value) {
+        while(value.contains("(")) {
+            int openIndex = value.lastIndexOf("(");
+            int closeIndex = value.indexOf(")", openIndex);
+
+            String inner = value.substring(openIndex + 1, closeIndex);
+            int result = run(inner);
+
+            value = value.substring(0, openIndex) + result + value.substring(closeIndex + 1);
+        }
+        return value;
+    }
+
+
+    public static void inputList(List<String> valueList, String value) {
+        String[] bits = value.split(" ");
+
+        valueList.addAll(Arrays.asList(bits));
+
+    }
+
 
 }
